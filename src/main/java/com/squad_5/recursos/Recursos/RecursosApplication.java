40b5ba0027dde2e0bc45f86cb73b863b8e84884e
Iván Conde <iconde@coelsa.com.ar>
2022-06-20 17:19:30 -0300

    Seteo del application.properties para la bd y definicion de la arquitectura a utilizar

--- src/main/java/com/squad_5/recursos/Recursos/RecursosApplication.java
+++ src/main/java/com/squad_5/recursos/Recursos/RecursosApplication.java
@@ -2,19 +2,9 @@ package com.squad_5.recursos.Recursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 @SpringBootApplication
 public class RecursosApplication {
-
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello World!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecursosApplication.class, args);
