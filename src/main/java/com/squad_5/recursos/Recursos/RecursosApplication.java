008daba0210386502ca8672810377495ddb3897f
AgussAndrade <51175036+AgussAndrade@users.noreply.github.com>
2022-07-03 17:31:43 -0300

    add documentation

--- src/main/java/com/squad_5/recursos/Recursos/RecursosApplication.java
+++ src/main/java/com/squad_5/recursos/Recursos/RecursosApplication.java
@@ -1,9 +1,24 @@
 package com.squad_5.recursos.Recursos;
+import com.google.common.base.Predicates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class RecursosApplication {
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(Predicates.not(PathSelectors.regex("/error")))
+				.paths(Predicates.not(PathSelectors.regex("/")))
+				.paths(PathSelectors.any())
+				.build();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(RecursosApplication.class, args);
 	}
